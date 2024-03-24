53b35b577f251ed63ce91c28046483282fb070ee
oncelearner <redaamir972@gmail.com>
2024-03-24 13:48:36 +0100

    feat: use await for all

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -8,32 +8,46 @@ if (document.readyState == "complete") {
   };
 }
 
+// since observer.disconnect() caused the extension to stop working, let's embrace an async/wait approach.
+async function waitForElementAvailability(selector) {
+  while (!document.querySelector(selector)) {
+    await new Promise(resolve => requestAnimationFrame(resolve));
+  }
+  console.log("Caught!")
+  return document.querySelector(selector);
+}
+
+async function waitForBodyOverflowHidden() {
+  while (document.body.style.overflow!=='hidden') {
+    await new Promise(resolve => requestAnimationFrame(resolve));
+  }
+  document.body.style.overflow='scroll';
+  document.body.parentElement.style.overflow='scroll';
+  return ;
+}
+
+
+async function doMyStuffAfterLoad() {
+  const element = await waitForElementAvailability(".dgEhJe6g")
+  const popupWrapper = element.parentElement;
+  popupWrapper.remove()
+  // in case remove() didnt' work
+  popupWrapper.style.display = 'none';
+  waitForBodyOverflowHidden();
+}
+
+// For future ref:
+// For memory leaks concerns( since we don't disconnect the observer), let's go for the async wait apprach.
 function ObserveElementAvailability(selector, callback) {
-  const observer = new MutationObserver(mutations => {
-    console.log("new")
+  const observer = new MutationObserver( function(mutations) {
 
     if (document.querySelector(selector)) {
-      console.log("Caught!")
+      console.log("Observer Caught it!")
       // disconnecting the observer seems to break our work, 
       //I think the website ties to re-inject it many times.
       // observer.disconnect();
       callback();
     }
   });
-
   observer.observe(document.body, { childList: true, subtree: true });
-}
-
-
-function doMyStuffAfterLoad() {
-  ObserveElementAvailability(".dgEhJe6g", () => {
-    // Element-specific code here
-    const popupWrapper = document.querySelector('.dgEhJe6g')?.parentElement;
-    popupWrapper.remove()
-    // in case remove() didnt' work
-    popupWrapper.style.display = 'none';
-
-    document.body.parentElement.style.overflow = "scroll";
-    document.body.style.overflow = 'scroll'
-  });
 }
