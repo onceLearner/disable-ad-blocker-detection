c2d10bc1a24733e204976582c58458f74f23605b
oncelearner <redaamir972@gmail.com>
2024-03-24 12:54:17 +0100

    feat: use observer API instead of timeouts

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,35 +1,35 @@
-function doMyStuffAfterLoad() {
-    console.log("ready")
-    const container = document.getElementsByClassName('dgEhJe6g');
-    console.log(container)
-
-    const body = document.body.style.overflow='scroll'
-    waitForElement(".dgEhJe6g", () => {
-        // Element-specific code here
-        const popupWrapper = document.querySelector('.dgEhJe6g')?.parentElement;
-        popupWrapper.remove()
-        popupWrapper.style.display='none';
-        document.body.parentElement.style.overflow="scroll";
-        console.log(popupWrapper)
-        document.body.style.overflow='scroll'
-    });
-  }
-  
-  if (document.readyState == "complete") {
-    doMyStuffAfterLoad();
-  } else {
-    document.onreadystatechange = function() {
-      if (document.readyState == "complete") {
-        doMyStuffAfterLoad();
-      }
-    };
-  }
-  
-
-  function waitForElement(selector, callback) {
-    if (document.querySelector(selector)) {
-      callback();
-    } else {
-      setTimeout(() => waitForElement(selector, callback), 50);
+if (document.readyState == "complete") {
+  doMyStuffAfterLoad();
+} else {
+  document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+      doMyStuffAfterLoad();
     }
-  }
+  };
+}
+
+function ObserveElementAvailability(selector, callback) {
+  const observer = new MutationObserver(mutations => {
+    if (document.querySelector(selector)) {
+      console.log("Caught!")
+      observer.disconnect();
+      callback();
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+}
+
+
+function doMyStuffAfterLoad() {
+  ObserveElementAvailability(".dgEhJe6g", () => {
+    // Element-specific code here
+    const popupWrapper = document.querySelector('.dgEhJe6g')?.parentElement;
+    popupWrapper.remove()
+    // in case remove() didnt' work
+    popupWrapper.style.display = 'none';
+
+    document.body.style.overflow = 'scroll'
+    document.body.parentElement.style.overflow = "scroll";
+  });
+}
